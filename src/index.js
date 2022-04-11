ca910d835241cd82495180a359b956c9f42197ee
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-04-10 21:40:38 -0400

    Added date function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `Last updated: ${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +33,8 @@ function displayWeather(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = response.data.wind.speed;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c13ec1823489873786dad083e25adf72";
