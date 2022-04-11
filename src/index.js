2a4bf86764a6be989d7190dcf1ddb0b8ce6e4617
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-04-10 21:07:24 -0400

    Added weather display function

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function displayWeather(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "c13ec1823489873786dad083e25adf72";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
